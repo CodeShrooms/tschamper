84010e80d28a5ebfbee4618e1285495097371407
hoelzleva <eva-hoelzl@outlook.com>
2023-11-16 19:02:05 +0100

    initial options screen

--- scenes/main_screen/Menu.gd
+++ scenes/main_screen/Menu.gd
@@ -8,7 +8,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
@@ -17,10 +17,19 @@ func _on_new_game_pressed():
 
 
 func _on_options_pressed():
-	#var options = load("res://scenes/main_screen/OptionsScreen.tscn").instance()
-	#get_tree().current_scene.add_child(options)
-	pass
+	# Load the OptionsScreen scene and instance it
+	var options_instance = load("res://scenes/main_screen/OptionsScreen.tscn")
+
+	if options_instance:
+		# Add the OptionsScreen as a child to the current scene
+		get_tree().current_scene.add_child(options_instance)
+
+		# Set the OptionsScreen instance as the active scene
+		get_tree().current_scene = options_instance
+	else:
+		print("Failed to load OptionsScreen scene.")
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
