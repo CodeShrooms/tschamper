d15934e252bb3c784431244975783742170679a6
hoelzleva <eva-hoelzl@outlook.com>
2023-11-22 13:41:09 +0100

    change scene to options and back

--- scenes/main_screen/Menu.gd
+++ scenes/main_screen/Menu.gd
@@ -4,7 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#select New Game Button with keyboard
-	$VBoxContainer/NewGame.grab_focus()
+	$MainMenuScreen/VBoxContainer/NewGame.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,17 +17,8 @@ func _on_new_game_pressed():
 
 
 func _on_options_pressed():
-	# Load the OptionsScreen scene and instance it
-	var options_instance = load("res://scenes/main_screen/OptionsScreen.tscn")
-
-	if options_instance:
-		# Add the OptionsScreen as a child to the current scene
-		get_tree().current_scene.add_child(options_instance)
-
-		# Set the OptionsScreen instance as the active scene
-		get_tree().current_scene = options_instance
-	else:
-		print("Failed to load OptionsScreen scene.")
+	$OptionsScreen.visible = true
+	$MainMenuScreen.visible = false
 
 
 func _on_quit_pressed():
---
+++ scenes/main_screen/OptionsScreen.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_back_pressed():
+	var mainMenuScreen = $"../MainMenuScreen"
+	var currentScreen = $"."
+
+	if mainMenuScreen and currentScreen:
+		mainMenuScreen.visible = true
+		currentScreen.visible = false
+	else:
+		# Nodes don't exist, return to MainMenu
+		get_tree().change_scene_to_file("res://scenes/main_screen/Menu.tscn")
+
+
+func _on_save_pressed():
+	#TODO make save logic
+	$"../MainMenuScreen".visible = true
+	$".".visible = false
