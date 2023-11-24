2da4ea739829164f13281651cb8f05b8e21d68ac
hoelzleva <eva-hoelzl@outlook.com>
2023-11-24 16:22:50 +0100

    optimierter sound screen

--- scenes/main_screen/scenes/OptionsSoundScreen.gd
+++ scenes/main_screen/scenes/OptionsSoundScreen.gd
@@ -1,46 +1,40 @@
 extends Control
 
-const default_db = 10
+const default_db = 40
 
 const master_bus = 0
 const music_bus = 1
 const sfx_bus = 2
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	slider_default($CenterContainer/HBoxContainer/Slider/MasterVolumeSlider)
-	slider_default($CenterContainer/HBoxContainer/Slider/MusicVolumeSlider)
-	slider_default($CenterContainer/HBoxContainer/Slider/SFXVolumeSlider)
-
 
 func _on_master_default_pressed():
-	volume(master_bus, default_db)
-	slider_default($CenterContainer/HBoxContainer/Slider/MasterVolumeSlider)
+	set_volume(master_bus, default_db)
+	set_slider_default($CenterContainer/HBoxContainer/Slider/MasterVolumeSlider)
 
 
 func _on_music_default_pressed():
-	volume(music_bus, default_db)
-	slider_default($CenterContainer/HBoxContainer/Slider/MusicVolumeSlider)
+	set_volume(music_bus, default_db)
+	set_slider_default($CenterContainer/HBoxContainer/Slider/MusicVolumeSlider)
 
 
 func _on_sfx_default_pressed():
-	volume(sfx_bus, default_db)
-	slider_default($CenterContainer/HBoxContainer/Slider/SFXVolumeSlider)
+	set_volume(sfx_bus, default_db)
+	set_slider_default($CenterContainer/HBoxContainer/Slider/SFXVolumeSlider)
 
 
 func _on_master_volume_slider_value_changed(value):
-	volume(master_bus, value)
+	set_volume(master_bus, value)
 
 
 func _on_music_volume_slider_value_changed(value):
-	volume(music_bus, value)
+	set_volume(music_bus, value)
 
 
 func _on_sfx_volume_slider_value_changed(value):
-	volume(sfx_bus, value)
+	set_volume(sfx_bus, value)
 
-func volume(bus_index, volume):
+func set_volume(bus_index, volume):
 	AudioServer.set_bus_volume_db(bus_index, volume)
 
-func slider_default(slider_node):
+func set_slider_default(slider_node):
 	slider_node.value = 100
