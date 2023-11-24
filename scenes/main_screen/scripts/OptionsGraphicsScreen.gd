ee7dd8c47f1f7192b1fb4ef05487736a1d2424d0
hoelzleva <eva-hoelzl@outlook.com>
2023-11-24 12:14:07 +0100

    item box and checkboxes

--- scenes/main_screen/scripts/OptionsGraphicsScreen.gd
+++ scenes/main_screen/scripts/OptionsGraphicsScreen.gd
@@ -10,12 +10,10 @@ func _process(_delta):
 	pass
 
 func set_resolution(width, height):
-	# Your code to set the game resolution goes here
-	print("Setting resolution to:", width, "x", height)
-
+	get_viewport().size = Vector2i(width, height)
 
 func _on_option_button_item_selected(index):
-	var selected_option = $HBoxContainer/Checks/OptionButton.get_item_text(index)
+	var selected_option = $CenterContainer/HBoxContainer/Checks/ResolutionSelect.get_item_text(index)
 	match selected_option:
 		"1920x1080":
 			set_resolution(1920, 1080)
@@ -24,3 +22,10 @@ func _on_option_button_item_selected(index):
 		# Add more options as needed
 		_:
 			print("Unknown option selected:", selected_option)
+
+
+func _on_full_screen_toggled(button_pressed):
+	if button_pressed:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
