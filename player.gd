ee1fd713f0b61bb3e00c3f6f53b4d8454ab06566
filleo <felixmueller1620@gmail.com>
2023-12-08 21:32:54 +0100

    changed movement of the mob, added respawn for testing

--- player.gd
+++ player.gd
@@ -13,6 +13,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite
 
+var saved_position : Vector2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -62,10 +63,18 @@ func update_animation():
 			animated_sprite.play("idle")
 		
 
+# 
+func _unhandled_key_input(event):
+	if event.is_action_pressed("save_position"):
+		saved_position = self.position
+		
+
 # wenn irgendein Objekt aus den Collision Masks die Area betritt, wird der Spieler gelöscht
 func _on_area_2d_body_entered(_body):
-	queue_free() #austauschen durch eine Sterbefunktion
+	die()
 	
-
+#was passieren soll, wenn der Player "stirbt". wird zurück zu einem gespeicherten Ort gesetzt
+func die():
+	self.position = saved_position
 
 	
--- testmob.gd
+++ testmob.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 30.0
-var direction = 1
+var direction = -1
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,11 +15,13 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	# changes direction when hitting a wall
 	if is_on_wall(): 
+		scale.x = -scale.x
 		direction = direction * -1 
 		
 	# changes direction when on a edge
-	if not is_on_floor():
-		direction = direction * -1
+	if (not $RayCast2D.is_colliding() and is_on_floor()):
+		scale.x = - scale.x #mirrors mob 
+		direction = direction * -1 #mirrors movement direction
 	velocity.x = direction * SPEED
 	
 	# var query = PhysicsRayQueryParameters2D.create(Vector2(0,0), Vector2(50,100))
