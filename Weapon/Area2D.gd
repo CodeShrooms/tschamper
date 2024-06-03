4b3e3d72a64be3923e23c25315f6b4e1c27aa73e
JESS-L-ROBOT <zickgrafj@gmail.com>
2024-06-03 18:45:28 +0200

    Kommentar verbessert bei if area is enemy

--- Weapon/Area2D.gd
+++ Weapon/Area2D.gd
@@ -15,7 +15,8 @@ func _physics_process(delta):
 func _on_area_shape_entered(_area_rid, area, _area_shape_index, _local_shape_index):
 	if area is Enemy:
 		area.take_damage(damage)
-		queue_free()  # Bullet dissapers when hit (optional)
+		queue_free() # Bullet dissapers when hit # Ideally, this should happen within the function _on_body_entered(_body), but we will handle it 
+					 # here explicitly to ensure the required actions are executed correctly
 
 func _on_body_entered(_body): #activate if not wanted to pass any body 
 	queue_free() #prevents bullet form passing through walls , sprits, npcs ... etc
